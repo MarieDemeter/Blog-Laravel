61be4f840ba43be3416da6b5d1ce75f015193a59
Marie-Demeter <mdemeter.ducrot@gmail.com>
2022-07-27 11:35:46 +0200

    feat: :zap: Print number of comments for each article

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,12 @@
         <div>
             <h2 class="text-xl"> {{$article->title}} </h2>
             @php 
+                $nbOfComments = $article->comments()->count();
                 $article = Str::limit($article->content, $limit = 500, $end = '(...)');
                 $nbWordsArticle = Str::wordCount($article);
             @endphp
             <p> {{Str::words($article, $nbWordsArticle - 1, ' [...]')}} </p>
+            <p class="text-center">Il y a {{$nbOfComments}} sur cet article.</p>
             </br>
         </div>
         @endforeach
