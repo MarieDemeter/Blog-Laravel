6581abfc0b0d4cd90a106b1256259074b4ae9f9f
MarieDemeter <mdemeter.ducrot@gmail.com>
2022-08-02 14:18:57 +0200

    test: :zap: Add new tests

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -38,17 +38,42 @@ class ApiArticleTest extends TestCase
         $this->assertDatabaseCount('articles', 100);
     }
 
+    public function test_article_status()
+    {
+        $response = $this->getJson('/api/article/99');
+        $response->assertStatus(200);
+    }
+
+    public function test_articles_status()
+    {
+        $response = $this->getJson('/api');
+        $response->assertStatus(200);
+    }
+
+    public function test_comment_status()
+    {
+        $response = $this->postJson('/api/comment', [
+            'article_id' => 1,
+            'content' => 'bla bla bla',
+            'pseudo' => 'test',
+            'email' => 'test@test.fr', 
+        ]);
+        $response->assertStatus(201);
+    }
+
+
     public function test_the_api_article_returns_a_JSON_file_with_correct_data()
     {
-        $article_un = Article::find(99);
+        $article = Article::find(99);
 
         $response = $this->getJson('/api/article/99');
+        
         $response->assertJson(
             fn (AssertableJson $json) =>
             $json->where('id', 99)
-                ->where('title', $article_un->title)
-                ->where('content', $article_un->content)
-                ->where('user_id', $article_un->user_id)
+                ->where('title', $article->title)
+                ->where('content', $article->content)
+                ->where('user_id', $article->user_id)
                 ->etc()
         );
     }
@@ -71,25 +96,45 @@ class ApiArticleTest extends TestCase
                     ->where('content', $firstArticle->content)
                     ->where('user_id', $firstArticle->user_id)
                     ->etc()
-            )->etc()
+            )
+            ->etc()
         );
     }
 
-    public function test_the_api_comment_returns_a_JSON_file_with_correct_data()
+
+    public function test_the_api_articles_returns_a_JSON_file_with_all_informations()
     {
         $articles_count_PerPage = 10;
         $firstArticle = Article::latest()->first();
 
+        $response = $this->getJson('/api');
+
+        $response->assertJson(
+            fn (AssertableJson $json) =>
+            $json->hasAll('current_page','data','from','last_page','last_page_url','links','next_page_url','path','per_page','prev_page_url','to','total')
+                 ->etc()
+        );
+    }
+
+
+    public function test_the_api_comment_returns_a_JSON_file_with_correct_data()
+    {
         $response = $this->postJson('/api/comment', [
             'article_id' => 1,
             'content' => 'bla bla bla',
             'pseudo' => 'test',
-            'email' => 'test@test.fr',
+            'email' => 'test@test.fr', 
         ]);
 
         $response->assertJsonPath('article_id',1);
         $response->assertJsonPath('content','bla bla bla');
         $response->assertJsonPath('pseudo','test');
         $response->assertJsonPath('email','test@test.fr');
+    }
+
+    public function test_article_404_error()
+    {
+        $response = $this->getJson('/api/article/test');
+        $response->assertStatus(404);
     }
 }
