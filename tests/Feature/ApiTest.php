66f7a6aa2f56a5a4685c3564309df74d0d843939
MarieDemeter <mdemeter.ducrot@gmail.com>
2022-08-02 14:21:16 +0200

    refactor: :art: refacto Pint test

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -5,8 +5,6 @@ namespace Tests\Feature;
 use App\Models\Article;
 use App\Models\User;
 use Database\Seeders\ArticleSeeder;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
@@ -56,21 +54,19 @@ class ApiArticleTest extends TestCase
             'article_id' => 1,
             'content' => 'bla bla bla',
             'pseudo' => 'test',
-            'email' => 'test@test.fr', 
+            'email' => 'test@test.fr',
         ]);
         $response->assertStatus(201);
     }
-
 
     public function test_the_api_article_returns_a_JSON_file_with_correct_data()
     {
         $article = Article::find(99);
 
         $response = $this->getJson('/api/article/99');
-        
+
         $response->assertJson(
-            fn (AssertableJson $json) =>
-            $json->where('id', 99)
+            fn (AssertableJson $json) => $json->where('id', 99)
                 ->where('title', $article->title)
                 ->where('content', $article->content)
                 ->where('user_id', $article->user_id)
@@ -86,12 +82,10 @@ class ApiArticleTest extends TestCase
         $response = $this->getJson('/api');
 
         $response->assertJson(
-            fn (AssertableJson $json) =>
-            $json->has(
+            fn (AssertableJson $json) => $json->has(
                 'data',
                 $articles_count_PerPage,
-                fn ($json_data) =>
-                $json_data->where('id', $firstArticle->id)
+                fn ($json_data) => $json_data->where('id', $firstArticle->id)
                     ->where('title', $firstArticle->title)
                     ->where('content', $firstArticle->content)
                     ->where('user_id', $firstArticle->user_id)
@@ -101,7 +95,6 @@ class ApiArticleTest extends TestCase
         );
     }
 
-
     public function test_the_api_articles_returns_a_JSON_file_with_all_informations()
     {
         $articles_count_PerPage = 10;
@@ -110,12 +103,10 @@ class ApiArticleTest extends TestCase
         $response = $this->getJson('/api');
 
         $response->assertJson(
-            fn (AssertableJson $json) =>
-            $json->hasAll('current_page','data','from','last_page','last_page_url','links','next_page_url','path','per_page','prev_page_url','to','total')
+            fn (AssertableJson $json) => $json->hasAll('current_page', 'data', 'from', 'last_page', 'last_page_url', 'links', 'next_page_url', 'path', 'per_page', 'prev_page_url', 'to', 'total')
                  ->etc()
         );
     }
-
 
     public function test_the_api_comment_returns_a_JSON_file_with_correct_data()
     {
@@ -123,13 +114,13 @@ class ApiArticleTest extends TestCase
             'article_id' => 1,
             'content' => 'bla bla bla',
             'pseudo' => 'test',
-            'email' => 'test@test.fr', 
+            'email' => 'test@test.fr',
         ]);
 
-        $response->assertJsonPath('article_id',1);
-        $response->assertJsonPath('content','bla bla bla');
-        $response->assertJsonPath('pseudo','test');
-        $response->assertJsonPath('email','test@test.fr');
+        $response->assertJsonPath('article_id', 1);
+        $response->assertJsonPath('content', 'bla bla bla');
+        $response->assertJsonPath('pseudo', 'test');
+        $response->assertJsonPath('email', 'test@test.fr');
     }
 
     public function test_article_404_error()
